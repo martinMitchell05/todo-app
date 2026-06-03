7cd3e2551af96845cef4415129ca079747f49012
Mitchell Martin <martinmitchellmartin95@gmail.com>
2026-06-03 00:08:25 -0300

    codigo inicial, prueba de ventana

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 import sys
-from ui.main_window import MainWindow
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
 # Modulos principales de PyQt6 ---> QtWidgets, QtCore, QtGui
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__() # cuando se crea una subclase de Qt siempre se debe inicializar el super() para que Qt construya el objeto correctamente
+        
+        self.setWindowTitle("Mi To-Do App")
+        self.setGeometry(860, 240, 480, 640) # x, y, ancho, alto
+        
+        # Un texto de prueba en el medio
+        label = QLabel("Entorno listo.", self)
+        label.move(int(self.width()/2), int(self.height()/2))
+
+        # Ejemplo de como crear un botón
+        # button = QPushButton("Submit")
+        # self.setCentralWidget(button) --> establece al boton en toda la ventana
+
 
 
 if __name__ == "__main__":
